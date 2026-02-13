29ef2562ae16bc8bbb8e22478638653d5a9a64d9
Harman-jot-Kaur <harmanjotkaurr01@gmail.com>
2026-02-13 13:47:05 -0600

    feat: update response

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello! This is a flower app.</h1>");
 });
 
+app.get("/flower", (req, res) => {
+  res.json({
+    name: "Rose",
+    color: "Red",
+    fragrance: "Sweet",
+    image_url: "https://flower-harman.s3.us-east-1.amazonaws.com/download.jpg"
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
